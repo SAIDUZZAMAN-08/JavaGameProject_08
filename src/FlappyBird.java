1a3bebe7d64016851b336a54b7196b107d132adc
SAIDUZZAMAN-08 <saiduzzamansajeeb71@gmail.com>
2025-05-19 03:11:37 +0600

    Load Images

--- src/FlappyBird.java
+++ src/FlappyBird.java
@@ -1,14 +1,29 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class FlappyBird extends JPanel {
 
     int boardWidth = 360;
     int boardHeight = 640;
 
+    // Images
+    Image backgroundImg, birdImg, topPipeImg, bottomPipeImg;
+
     public FlappyBird() {
         setPreferredSize(new Dimension(boardWidth, boardHeight));
         setFocusable(true);
+
+
+        // Load Images
+        backgroundImg = new ImageIcon(getClass().getResource("./flappybirdbg.png")).getImage();
+        birdImg = new ImageIcon(getClass().getResource("./flappybird.png")).getImage();
+        topPipeImg = new ImageIcon(getClass().getResource("./toppipe.png")).getImage();
+        bottomPipeImg = new ImageIcon(getClass().getResource("./bottompipe.png")).getImage();
+
+
     }
 
     public static void main(String[] args) {
